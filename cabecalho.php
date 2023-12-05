6c503ecaceff43b836830c554e7793cb3996780b
IkaroJS <ikarojesusdasilva@outlook.com>
2023-12-05 19:09:10 -0300

    Sem alteração

---
+++ cabecalho.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/4b3d25dac2.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+ <header>
+ <div id="header">
+    <div class="navbar">    
+        <ul>
+    
+            <li>
+                <a href="download_inserir.php" class="iconDownload" style="margin-left: 25px;">
+                    <i class="fa-solid fa-file-export" style="color: #002057;"></i>
+                </a>
+            </li>
+
+            <li>
+                <a href="index.php" class="iconHome" style="margin-right: 10px;">
+                    <i class="fa-solid fa-house-chimney" style="color: #002057;"></i>
+                </a>
+            </li>
+
+            <li>
+                <a href="calendario_exibir.php" class="iconCalendar" style="margin-right: 30px;">
+                    <i class="fa-solid fa-calendar-days" style="color: #002057;"></i>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+
+    <div class="topo">
+        <li>Arquivos</li>
+        <li>Início</li>
+        <li>Calendário</li>
+    </div>
+</div>
+
+<h1 style="text-align: center;">Site para organização pessoal</h1>
+</header>
+
+
+
+</body>
+</html>
